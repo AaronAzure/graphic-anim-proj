9e2a260f35fc8e88ed8f20e442b7731e0ed4c8ad
AaronAzure <azurakeni@gmail.com>
2022-04-24 18:54:39 +0800

    build + report

---
+++ build/CMakeFiles/angel.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/aaronwee/Desktop/UWA/graphic-anim-proj/CITS3003_Project_Template/lib/angel/src/InitShader.cpp" "CMakeFiles/angel.dir/src/InitShader.cpp.o" "gcc" "CMakeFiles/angel.dir/src/InitShader.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
